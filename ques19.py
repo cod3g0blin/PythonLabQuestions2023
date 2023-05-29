cbea7d014d12f562c95b916197c6485dd990027f
Shubham Biswas <shubhambiswas487@gmail.com>
2023-05-29 17:00:24 +0530

    Add files via upload

--- ques19.py
+++ ques19.py
@@ -1,3 +1,11 @@
-#write python function to count the occurence of user-entered words in a sentence
+#write Python function to count the number of occurence of user-entered words in a sentence.
+def OccurenceOfUserEnteredWords(sentence):
+    count = 0
+    sentence=sentence.replace("  ", " ")
+    for word in sentence:
+        if(word==" "):
+            count+=1
+    return count
 
-    
+sentence=input("Enter a sentence: ")
+print("No. of words = ", (OccurenceOfUserEnteredWords(sentence)+1))
